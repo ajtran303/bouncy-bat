743c0802b7f65232cf67a641edca17c1a9ee6514
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2026-07-10 02:33:32 -0400

    add osc. pipes

---
+++ Scenes/Main/spike_spawner.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var spike_scene: PackedScene
+@export var spawn_x: float = 550.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_spawn_timer_timeout() -> void:
+	var spike := spike_scene.instantiate()
+	spike.position.x = spawn_x
+	spike.oscillates = randf() < 0.33
+	add_child(spike)
---
+++ Scenes/SpikePair/spike_pair.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+@export var scroll_speed: float = 200.0
+
+@export var gap_size_min: float = 120.0
+@export var gap_size_max: float = 180.0
+@export var gap_y_min: float = 150.0
+@export var gap_y_max: float = 450.0
+
+@export var oscillates: bool = false
+@export var oscillation_speed: float = 2.0
+@export var oscillation_range: float = 50.0
+
+var _base_y: float
+var _elapsed: float = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var gap_center := randf_range(gap_y_min, gap_y_max)
+	var gap_size := randf_range(gap_size_min, gap_size_max)
+	var pipe_height := 320.0
+	$TopSpike.position.y = gap_center - gap_size / 2.0 - pipe_height
+	$BottomSpike.position.y = gap_center + gap_size / 2.0
+	_base_y = position.y
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	position.x -= scroll_speed * delta
+	if oscillates:
+		_elapsed += delta
+		position.y = _base_y + sin(_elapsed * oscillation_speed) * oscillation_range
+	if position.x < -100.0:
+		queue_free()
